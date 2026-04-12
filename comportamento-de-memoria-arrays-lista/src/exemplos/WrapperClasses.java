03c17667c1378441d67c38404f2e2458753ce6cf
Renato Nobrega <renatonobrega87@gmail.com>
2026-04-12 14:05:01 -0300

    implementando classe usando wrapper classes

---
+++ comportamento-de-memoria-arrays-lista/src/exemplos/WrapperClasses.java
@@ -0,0 +1,21 @@
+package exemplos;
+
+public class WrapperClasses {
+
+    static void main() {
+        
+        // São classes equivalentes aos tipos primitivos
+        
+        // Boxing e unboxing é natural na linguagem
+        
+        // Uso comum: campos de entidades em sistemas de informação (IMPORTANTE!)
+        
+        // Pois tipos referência (classes) aceitam valor null e usufruem dos recurso OO
+
+        // Demo
+
+        Integer x = 10;
+
+        int y = x * 2;
+    }
+}
