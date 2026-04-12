2e2e259477dce86f2fd23e629d8708749287b193
Renato Nobrega <renatonobrega87@gmail.com>
2026-04-12 14:08:36 -0300

    implementando exemplo de fixacao usando vetores em Java

--- comportamento-de-memoria-arrays-lista/src/exercicio_fixacao/Program.java
+++ comportamento-de-memoria-arrays-lista/src/exercicio_fixacao/Program.java
@@ -1,4 +1,34 @@
 package exercicio_fixacao;
 
-public class Exercicio {
+import java.util.Scanner;
+
+public class Program {
+    static void main() {
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("How many rooms will be rented? ");
+        int n = entrada.nextInt();
+        Rent[] vect = new Rent[10];
+
+        for (int i = 1; i <= n; i++) {
+            entrada.nextLine();
+            System.out.println("Rent # " + i + ": ");
+            System.out.print("Name: ");
+            String name = entrada.nextLine();
+            System.out.print("Email: ");
+            String email = entrada.nextLine();
+            System.out.print("Room: ");
+            int room = entrada.nextInt();
+            vect[room] = new Rent(name, email);
+        }
+
+        System.out.println();
+        System.out.println("Busy rooms:");
+        for (int i = 0; i < 10; i++) {
+            if (vect[i] != null) {
+                System.out.println(i + ": " + vect[i]);
+            }
+        }
+
+        entrada.close();
+    }
 }
--- comportamento-de-memoria-arrays-lista/src/exercicio_fixacao/Rent.java
+++ comportamento-de-memoria-arrays-lista/src/exercicio_fixacao/Rent.java
@@ -1,4 +1,41 @@
 package exercicio_fixacao;
 
 public class Rent {
+
+    private String name;
+    private String email;
+    private int room;
+
+    public Rent(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setRoom(int room) {
+        this.room = room;
+    }
+
+    public int getRoom() {
+        return room;
+    }
+
+    public String toString(){
+        return name + ", " + email;
+    }
 }
