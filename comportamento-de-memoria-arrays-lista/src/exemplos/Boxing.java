1cb572d9227c06e58d288df08db17a72234ce5e0
Renato Nobrega <renatonobrega87@gmail.com>
2026-04-12 14:03:06 -0300

    implementando exemplos usando os conceitos de boxing e unboxing

---
+++ comportamento-de-memoria-arrays-lista/src/exemplos/Boxing.java
@@ -0,0 +1,14 @@
+package exemplos;
+
+public class Boxing {
+
+    static void main() {
+
+        // Boxing
+
+        // É o processo de conversão de um objeto tipo valor para um objeto tipo referência compatível
+
+        int x = 20;
+        Object obj = x;
+    }
+}
---
+++ comportamento-de-memoria-arrays-lista/src/exemplos/Unboxing.java
@@ -0,0 +1,17 @@
+package exemplos;
+
+public class Unboxing {
+
+    static void main() {
+
+        // Unboxing
+
+        // É o processo de conversão de um objeto tipo referência para um objeto tipo valor compatível
+
+        int x = 20;
+
+        Object obj = x;
+
+        int y = (int) obj;
+    }
+}
