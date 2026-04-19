2f63c2372c5c1516e825800b5cdef975782189bb
Renato Nobrega <renatonobrega87@gmail.com>
2026-04-18 21:29:27 -0300

    Implementando exercicio resolvido sobre enum e composicao em Java

---
+++ enumeracoes-composicao/src/exercicio_resolvido/application/Program.java
@@ -0,0 +1,64 @@
+package exercicio_resolvido.application;
+
+import exercicio_resolvido.entities.Department;
+import exercicio_resolvido.entities.HourContract;
+import exercicio_resolvido.entities.Worker;
+import exercicio_resolvido.entities.WorkerLevel;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+    static void main() {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        IO.print("Entre com o nome do departamento: ");
+        String departmentName = sc.nextLine();
+
+        Department department = new Department(departmentName);
+
+        IO.println("Entre com os dados do trabalhado: ");
+        IO.print("Nome: ");
+        String workerName = sc.nextLine();
+        IO.print("Nivel: ");
+        WorkerLevel workerLevel = WorkerLevel.valueOf(sc.nextLine());
+        IO.print("Salário base: ");
+        double baseSalary = sc.nextDouble();
+
+        Worker worker = new Worker(workerName, workerLevel, baseSalary, department);
+
+        IO.print("Quantos contratos esse trabalhador tem? ");
+        int n =  sc.nextInt();
+
+        for(int i = 1; i <= n; i++) {
+            IO.println("Entre com os dados do contrato #" + i + ":");
+            IO.print("Data (DD/MM/YYYY): )");
+            LocalDate contractDate = LocalDate.parse(sc.next(), fmt);
+            IO.print("Valor por hora: ");
+            double valuePerHour = sc.nextDouble();
+            IO.print("Duração (horas): ");
+            int hours = sc.nextInt();
+            HourContract contract = new HourContract(contractDate, valuePerHour, hours);
+            worker.addContract(contract);
+        }
+
+        IO.println();
+        IO.print("Entre o ano e mes para calcular o ganho (MM/YYYY) :");
+        String monthAndYear = sc.next();
+
+        int month = Integer.parseInt(monthAndYear.substring(0, 2));
+        int year = Integer.parseInt(monthAndYear.substring(3));
+
+        IO.println("Nome: " + worker.getName());
+        IO.println("Departamento: " + worker.getDepartment().getName());
+        IO.println("Ganho para " + monthAndYear + ": " + String.format("%.2f" , worker.income(month, year)));
+
+        sc.close();
+    }
+}
---
+++ enumeracoes-composicao/src/exercicio_resolvido/entities/WorkerLevel.java
@@ -0,0 +1,8 @@
+package exercicio_resolvido.entities;
+
+public enum WorkerLevel {
+
+    JUNIOR,
+    MID_LEVEL,
+    SENIOR
+}
