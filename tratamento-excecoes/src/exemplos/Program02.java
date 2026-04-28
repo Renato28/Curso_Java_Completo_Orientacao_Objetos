d0d61b9592b38db3e99404dbde47be628b142d34
Renato Nobrega <renatonobrega87@gmail.com>
2026-04-28 11:28:30 -0300

    implementando exemplo usando pilha de chamada de metodos

---
+++ tratamento-excecoes/src/exemplos/Program02.java
@@ -0,0 +1,39 @@
+package exemplos;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Program02 {
+
+    static void main() {
+
+        method1();
+
+        System.out.println("End of program");
+    }
+
+    private static void method1() {
+        System.out.println("***METHOD1 START");
+        method2();
+        System.out.println("***METHOD1 END");
+    }
+
+    private static void method2() {
+        System.out.println("***METHOD2 START");
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            String[] vect = sc.nextLine().split(" ");
+            int position = sc.nextInt();
+            System.out.println(vect[position]);
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid position");
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Input error");
+        }
+        sc.close();
+        System.out.println("***METHOD2 END");
+    }
+}
