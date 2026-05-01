b406eb5c1af66ab18986da93817eb0c7df116c26
Renato Nobrega <renatonobrega87@gmail.com>
2026-05-01 12:32:09 -0300

    Informações do caminho do arquivo

---
+++ trabalhando-com-arquivos/src/Exemplos/Program06.java
@@ -0,0 +1,23 @@
+package Exemplos;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Program06 {
+
+    static void main() {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a folder path: ");
+        String strPath = sc.nextLine();
+
+        File path = new File(strPath);
+
+        System.out.println("getPath: " + path.getPath());
+        System.out.println("getParent: " + path.getParent());
+        System.out.println("getName: " + path.getName());
+
+        sc.close();
+    }
+}
